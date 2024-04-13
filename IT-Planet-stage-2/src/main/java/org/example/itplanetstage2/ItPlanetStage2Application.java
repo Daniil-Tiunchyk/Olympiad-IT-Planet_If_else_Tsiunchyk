60d758ec7f2526fa99352d4d11cef036aa27868e
Daniil-Tsiunchyk <fcad.td@gmail.com>
2024-04-13 15:50:30 +0300

    init project

---
+++ IT-Planet-stage-2/src/main/java/org/example/itplanetstage2/ItPlanetStage2Application.java
@@ -0,0 +1,13 @@
+package org.example.itplanetstage2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItPlanetStage2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItPlanetStage2Application.class, args);
+    }
+
+}
